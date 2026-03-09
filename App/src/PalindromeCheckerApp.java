9abb6ac8ad6e6b1ed590e0e9ebcb1d8cd87714bb
shweta12chauhan <sc3232@srmist.edu.in>
2026-03-09 15:22:23 +0530

    [Ankit] Added [UC8]

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,33 +1,78 @@
-import java.util.Deque;
-import java.util.ArrayDeque;
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-                // Hardcoded string
-                String text = "level";
 
-                // Create deque
-                Deque<Character> deque = new ArrayDeque<>();
+            // Node class for singly linked list
+            static class Node {
+                char data;
+                Node next;
 
-                // Insert characters into deque
-                for (int i = 0; i < text.length(); i++) {
-                    deque.addLast(text.charAt(i));
+                Node(char data) {
+                    this.data = data;
+                    this.next = null;
+                }
+            }
+
+            // Function to reverse linked list
+            public static Node reverse(Node head) {
+                Node prev = null;
+                Node current = head;
+
+                while (current != null) {
+                    Node next = current.next;
+                    current.next = prev;
+                    prev = current;
+                    current = next;
+                }
+                return prev;
+            }
+
+            // Function to check palindrome
+            public static boolean isPalindrome(Node head) {
+
+                if (head == null || head.next == null)
+                    return true;
+
+                // Find middle of list
+                Node slow = head;
+                Node fast = head;
+
+                while (fast.next != null && fast.next.next != null) {
+                    slow = slow.next;
+                    fast = fast.next.next;
                 }
 
-                boolean isPalindrome = true;
+                // Reverse second half
+                Node secondHalf = reverse(slow.next);
 
-                // Compare front and rear characters
-                while (deque.size() > 1) {
-                    char first = deque.removeFirst();
-                    char last = deque.removeLast();
+                // Compare halves
+                Node firstHalf = head;
+                Node temp = secondHalf;
 
-                    if (first != last) {
-                        isPalindrome = false;
-                        break;
+                while (temp != null) {
+                    if (firstHalf.data != temp.data) {
+                        return false;
                     }
+                    firstHalf = firstHalf.next;
+                    temp = temp.next;
                 }
 
-                // Print result
-                if (isPalindrome) {
+                return true;
+            }
+
+            public static void main(String[] args) {
+
+                String text = "radar";
+
+                // Convert string to linked list
+                Node head = new Node(text.charAt(0));
+                Node current = head;
+
+                for (int i = 1; i < text.length(); i++) {
+                    current.next = new Node(text.charAt(i));
+                    current = current.next;
+                }
+
+                // Check palindrome
+                if (isPalindrome(head)) {
                     System.out.println("The string \"" + text + "\" is a palindrome.");
                 } else {
                     System.out.println("The string \"" + text + "\" is not a palindrome.");
