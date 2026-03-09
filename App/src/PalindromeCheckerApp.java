168d7c45b71422ca6f12ad19aeb8128883753714
shweta12chauhan <sc3232@srmist.edu.in>
2026-03-09 14:17:05 +0530

    [Ankit] Added [UC1]

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+
+        // Application details
+        String appName = "My Application";
+        String appVersion = "Version 1.0";
+
+        // Display welcome message
+        System.out.println("Welcome to " + appName);
+        System.out.println(appVersion);
+
+        // Continue to next process or exit
+        System.out.println("Application started successfully.");
+    }
+}
+
